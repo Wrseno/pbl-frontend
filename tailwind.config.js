c162451c4e9537a39148dbb0c57436ad42743861
Wrseno <warsenosetyono@gmail.com>
2024-10-16 06:47:57 +0700

    feat: add style css and tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1886EA",
+        secondary: "#53A6FF",
+        tertiary: "#FAAD14",
+      },
+      dropShadow: {
+        wh: "0 10px 10px rgba(200, 200, 255, 0.2)",
+      },
+    },
+  },
+};
