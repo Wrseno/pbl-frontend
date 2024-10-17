e064ffb9fc8b0f8d3de4a64e041a195a0f791fec
Wrseno <warsenosetyono@gmail.com>
2024-10-17 20:56:46 +0700

    feat: add infinite scroll animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,25 @@
 tailwind.config = {
   theme: {
     extend: {
+      animation: {
+        ["infinite-slider"]: "infiniteSlider 20s linear infinite",
+        ["infinite-slider-reverse"]:
+          "infiniteSliderReverse 20s linear infinite",
+      },
+      keyframes: {
+        infiniteSlider: {
+          "0%": {transform: "translateX(0)"},
+          "100%": {
+            transform: "translateX(calc(-250px * 5))",
+          },
+        },
+        infiniteSliderReverse: {
+          "0%": {transform: "translateX(calc(-250px * 5))"},
+          "100%": {
+            transform: "translateX(0)",
+          },
+        },
+      },
       colors: {
         primary: "#1886EA",
         secondary: "#53A6FF",
