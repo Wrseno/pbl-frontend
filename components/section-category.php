4bbcbd248d07f48d9fd713049611fabe47e903b4
Wrseno <warsenosetyono@gmail.com>
2024-11-08 10:39:30 +0700

    feat: menambahkan perubahan pada section category

--- components/section-category.php
+++ components/section-category.php
@@ -3,14 +3,6 @@
     <h4 class="text-xl poppins-bold">Cari berdasarkan</h4>
     <h2 class="text-3xl poppins-bold text-tertiary">Kategori Populer</h2>
   </div>
-  <div class="absolute top-0 right-0">
-    <a
-      href="#"
-      class="p-2 px-6 bg-gradient-to-r from-primary to-secondary text-white rounded-full font-semibold"
-    >
-      View All
-    </a>
-  </div>
   <div
     class="group p-4 relative grid gap-y-6 m-auto w-[320px] sm:w-[1000px] overflow-hidden bg-white before:absolute before:left-0 before:top-0 before:z-[2] before:h-full before:w-[100px] before:bg-[linear-gradient(to_right,white_0%,rgba(255,255,255,0)_100%)] before:content-[''] after:absolute after:right-0 after:top-0 after:z-[2] after:h-full after:w-[100px] after:-scale-x-100 after:bg-[linear-gradient(to_right,white_0%,rgba(255,255,255,0)_100%)] after:content-['']"
   >
