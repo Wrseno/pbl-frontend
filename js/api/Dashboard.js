b15d08c68e67f19c315c5b0ccf3b922a0a2a4e49
Wrseno <warsenosetyono@gmail.com>
2024-12-15 21:14:42 +0700

    feat: menambahkan api get info user

--- js/api/Dashboard.js
+++ js/api/Dashboard.js
@@ -1,28 +1,45 @@
-import {getSession} from "../utils.js";
+import {getProfileUser} from "../utils.js";
 import {API_BASE_URL} from "../config.js";
 
-const totalJoinedEvent = document.getElementById("total-joined-event");
 const currentlyJoinedEvent = document.getElementById("currently-joined-event");
-const username = document.getElementById("username");
-const session = await getSession();
-const userId = session.data.users_id;
+const hasPresentEvent = document.getElementById("has-present-event");
+const totalJoinedEvent = document.getElementById("total-joined-event");
+const totalEventHasPresent = document.getElementById("total-event-has-present");
+const usernameText = document.getElementById("username-dashboard");
 
 (async () => {
-  try {
-    const response = await fetch(
-      `${API_BASE_URL}/registration?user_id=${userId}`
-    );
-    const {data} = await response.json();
+  const {username, userId} = await getProfileUser();
 
-    if (data) {
-      const event = data[0];
+  try {
+    const eventJoinedResponse = await fetch(
+      `${API_BASE_URL}/registration?user_id=${userId}&not_present=true`
+    );
+    const eventHasPresentResponse = await fetch(
+      `${API_BASE_URL}/registration?user_id=${userId}&has_present=true`
+    );
+
+    const {data: eventJoined} = await eventJoinedResponse.json();
+    const {data: eventHasPresent} = await eventHasPresentResponse.json();
+
+    if (eventJoined) {
+      const event = eventJoined[0];
       currentlyJoinedEvent.textContent = event.title;
     } else {
       currentlyJoinedEvent.textContent = "Tidak ada.";
     }
 
-    totalJoinedEvent.textContent = data ? data.length : 0;
-    username.textContent = session.data.username;
+    if (eventHasPresent) {
+      const event = eventHasPresent[0];
+      hasPresentEvent.textContent = event.title;
+    } else {
+      hasPresentEvent.textContent = "Tidak ada event yang telah diikuti.";
+    }
+
+    totalJoinedEvent.textContent = eventJoined ? eventJoined.length : 0;
+    totalEventHasPresent.textContent = eventHasPresent
+      ? eventHasPresent.length
+      : 0;
+    usernameText.textContent = username;
   } catch (err) {
     console.error(err);
   }
